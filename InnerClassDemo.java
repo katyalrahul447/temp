a5c956d881ad8ae23ecc7896daff4d7a42e75ad2
Rahul Katyal <Rahul Katyal>
2017-01-30 19:25:20 +0530

    Changes to Inner Demo on open branch

--- InnerClassDemo.java
+++ InnerClassDemo.java
@@ -1,18 +1,28 @@
 class Outer {
-    int a = 10;
+   static int a = 10;
 
-    void show() {
-        System.out.println("Value of a: "+ a);
-        System.out.println("Value of b: "+ obj.b); 
-    }
-    class Inner {
+//    void disp() {
+//        Inner in = new Inner();
+//        in.show();
+//    }
+   static class Inner {   
         int b = 20;  //instance variable
+        static void show() {
+            System.out.println("Value of a: "+ a);
+            System.out.println("Value of b: "+ b); 
+        }        
     }
 }
 
 
 class Main {
     public static void main(String[] arg) {
-        Outer obj = new Outer();
+        // Outer out = new Outer();
+        // out.disp();
+        // Outer.Inner in = out.new Inner();
+        // in.show();
+
+        Outer.Inner in1 = new Outer.Inner();
+        // in1.show();
     }
 }
